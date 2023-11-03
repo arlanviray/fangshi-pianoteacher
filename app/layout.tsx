28e99597d049d7257a802002f3824f1b29311f40
arlanviray <arlan.viray@gmail.com>
2023-11-03 02:36:10 +0000

    added Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ const inter = Inter({ subsets: ["latin"] })
 // layouts
 import Header from "./layouts/Header"
 import Footer from "./layouts/Footer"
+// google tag manager
+import Script from "next/script"
+const GTM_ID = "GTM-K9W9SHR8"
 
 export default function RootLayout({
   children,
@@ -20,10 +23,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Header />
         <main>{children}</main>
         <Footer />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </html>
   )
